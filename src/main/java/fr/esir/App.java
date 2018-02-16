9c8ab46f479013270b034141422f6644489d781b
Vincent <vincent.bonhomme@etudiant.univ-rennes1.fr>
2018-02-16 11:34:41 +0100

    first commit

---
+++ src/main/java/fr/esir/App.java
@@ -0,0 +1,17 @@
+package fr.esir;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    /**
+     * Ceci est un commentaire javadoc
+     *
+     * @param args les arguments de la cmd
+     */
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
