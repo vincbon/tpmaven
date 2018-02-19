4a438439a81814aff1ef9140249966c3aa02c524
Vincent <vincent.bonhomme@etudiant.univ-rennes1.fr>
2018-02-19 15:49:59 +0100

    manifest

--- src/main/java/fr/esir/App.java
+++ src/main/java/fr/esir/App.java
@@ -12,6 +12,6 @@ public class App {
      * @param args les arguments de la cmd
      */
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! yay" );
     }
 }
